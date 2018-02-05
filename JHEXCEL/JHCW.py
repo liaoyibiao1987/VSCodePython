d75b4c010bc5db53ee669e1b82de6bce3af349ae
liaoyibiao1987 <zlyun616@gmail.com>
2018-02-05 12:14:52 +0800

    sssss

--- JHEXCEL/JHCW.py
+++ JHEXCEL/JHCW.py
@@ -1,20 +1,51 @@
+# -*- coding=utf-8 -*-
+# coding=utf-8
 import xlrd
+import configparser
+
+
+# def getconfig(path):
+#     cf = configparser.ConfigParser()
+#     cf.read(path)
+#     for item in cf.sections():
+#         # peersec = cf.items(item)
+#         inioptions = cf.options(item)
+#         for opt in inioptions:
+#             val = cf.get(item, opt)
+#             print(item, opt, val)
+#     pass
+
+
+def getJHconfig(path):
+    cf = configparser.ConfigParser()
+    cf.read(path)
+    ret = {}
+    for item in (['ValidColumns', 'ColumnsIndex'], ['ValidDataType', 'ColumnsDataType']):
+        val = cf.get(item[0], item[1])
+        ret[item[1]] = val
+    return ret
 
 def print_xls(path):
     data = xlrd.open_workbook(path)
-    table = data.sheets()[0] 
+    table = data.sheets()[0]
     nrows = table.nrows
     books = []
     for i in range(nrows):
-          ss = table.row_values(i)
-          if "book" in ss:
-              books.append(ss)
+        ss = table.row_values(i)
+        if "book" in ss:
+            books.append(ss)
     sum = 0
-    for i,item in enumerate(books):
-        print (int(i+1),item[0],item[1],int(item[2]),int(item[3]))
-        sum +=item[3]
+    for i, item in enumerate(books):
+        print(int(i+1), item)
+        sum += item[3]
 
-    print ("total amount is：",int(sum))
+    print("total amount is：", int(sum))
+
+def importexcle(inipath, exclepath):
+    configs = getJHconfig(inipath)
+    if(len(configs) > 0):
+        print(configs)
+
 
 if __name__ == "__main__":
-    print_xls('demo.xls')
+    importexcle('import.ini', 'demo.xls')
--- JHEXCEL/envs.md
+++ JHEXCEL/envs.md
@@ -1,5 +1,5 @@
 ####<font color=red>虚拟环境用pip安装</font>
->D:\Vscodes\JH\JHEXCEL\.virtualenv\Scripts>python -m pip3 install xlrd
+>D:\Vscodes\JH\JHEXCEL\\.virtualenv\Scripts>python -m pip install xlrd
 
 ####<font color=red>虚拟环境用安装</font>
 >```
@@ -7,4 +7,30 @@
 >virtualenv --distribute nowamagic_venv
 >virtualenv nowamagic_venv --no-site-packages 
 >   (--no-site-packages 选项的virtualenv就不会读取系统包)
+>```
+
+####<font color=red>Python snippets使用快捷键</font>
+>```
+>env	#!/usr/bin/env python
+>env3	#!/usr/bin/env python3
+>enc	# -- coding=utf-8 --
+>enco	# coding=utf-8
+>fenc	from future import ...
+>fenco	from future import ... (no -*-)
+>im	import
+>fim	from ... import ...
+>class	New class
+>defs	New method
+>def	New function
+>property	New property
+>if	if
+>for	for
+>while	while
+>try	try:except:
+>tryef	try:except:else:finally:
+>trye	try:except:else:
+>tryf	try:except:finally:
+>.	self
+>__	magic
+>ifmain	if name == "main"
 >```
